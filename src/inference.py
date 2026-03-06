7d710ff4cc8fe43d94ebddbef1ffa8e4c688663d
MaheswarRamS <bt25d030@smail.iitm.ac.in>
2026-03-06 22:34:11 +0530

    Update inference.py

--- src/inference.py
+++ src/inference.py
@@ -9,7 +9,7 @@ from sklearn.model_selection import train_test_split
 import tensorflow as tf
 from tensorflow import keras
 import argparse
-from neural_network import MLP, loss_and_grad
+from ann import NeuralNetwork as MLP, loss_and_grad
 
 
 def parse_arguments(args=None):   
