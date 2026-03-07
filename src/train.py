0f14bc352f307ccda4925410c3a7fdd3451e38ee
MaheswarRamS <bt25d030@smail.iitm.ac.in>
2026-03-07 09:39:33 +0530

    Update train.py

--- src/train.py
+++ src/train.py
@@ -7,7 +7,8 @@ from sklearn.metrics import precision_recall_fscore_support
 import tensorflow as tf
 from tensorflow import keras
 from sklearn.metrics import classification_report
-from ann import NeuralNetwork as MLP, loss_and_grad, optimizer
+from ann.neural_network import NeuralNetwork as MLP, loss_and_grad, optimizer
+
 
 
 def load_data(data_s):
