889ea63fd0c5313cfbd7786489c6de09c16a6770
MaheswarRamS <bt25d030@smail.iitm.ac.in>
2026-03-07 14:04:34 +0530

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,12 +1,5 @@
 import sys
 import os
-import subproces
 
-root = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, root)
-sys.path.insert(0, os.path.join(root, 'src'))
-
-subprocess.run(
-    [sys.executable, '-m', 'pip', 'install', '-e', root, '--quiet'],
-    check=False
-)
+sys.path.insert(0, '/autograder/source')
+sys.path.insert(0, '/autograder/source/src')
